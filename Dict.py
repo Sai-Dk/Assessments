2f4a5358c546d4181df88df1db00a0ef8a51190e
Sai-Dk <sai.dadvai@accellor.com>
2024-07-16 14:44:49 +0530

    Create Dict.py

---
+++ Dict.py
@@ -0,0 +1,13 @@
+info={
+    "name":" ",
+    "age":None,
+    "city":"",
+    "occupation":"Martial arts fighter"
+} 
+
+info["name"]=input("Enter your name: ")
+info["age"]=int(input("Enter your age: "))
+info["city"]=input("Enter your city: ")
+
+print(f"The information about {info["name"]} is his age is {info["age"]}, he is from {info["city"]} and He is a Martial arts fighter ")
+ 
