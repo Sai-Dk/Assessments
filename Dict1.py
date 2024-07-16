2e4d22d8b23c571aef1c74b09f51713f6eb56cf5
Sai-Dk <sai.dadvai@accellor.com>
2024-07-16 14:45:14 +0530

    Create Dict1.py

---
+++ Dict1.py
@@ -0,0 +1,10 @@
+Books={
+    "Dragon ball":"Akira toriyama",
+    "Mahabharat":"Ved vyas",
+    "Jojo's Bizzare adventure":"Araki",
+    "Gensin Impact": ""
+
+}
+Books["Genshin Impact"]="Mihoyo"
+
+print(f"My favoruite books and authors: ",Books)
