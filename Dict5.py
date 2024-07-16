345c3d4afd98efc6b4f91cd2d388ed1edfb9944c
Sai-Dk <sai.dadvai@accellor.com>
2024-07-16 14:47:41 +0530

    Create Dict5.py

---
+++ Dict5.py
@@ -0,0 +1,10 @@
+# given a dictionary representing the inventory of two stores(keys->item names and values->qunatities) 
+# create a function that merges these inventories into single dictionary if an item exists in both 
+# inventories sum the quantities
+inv1 = {"apple": 10, "banana": 5, "orange": 8}
+inv2 = {"banana": 3, "grape": 7, "apple": 2}
+
+def merged_inv(inv1):
+    merged_inventory={
+
+    }
