d0acba1c5410d12417e4a7f247ff07f561c7db47
Sai-Dk <sai.dadvai@accellor.com>
2024-07-24 16:31:38 +0530

    Create numpy 3*3.py

---
+++ numpy 3*3.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+matrix_3by3=np.array([[
+
+                   [2,3,4],
+                   [5,6,7],
+                   [8,9,10]
+    ]])
+
+print(matrix_3by3)
+
+
+
+
+
+
+
+# Write a NumPy program to create a 3x3 matrix with values ranging from 2 to 10.
