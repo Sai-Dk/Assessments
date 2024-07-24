c5870b600e31835da8937c350564e8d5bd1711f7
Sai-Dk <sai.dadvai@accellor.com>
2024-07-24 16:26:27 +0530

    Create numpy append.py

---
+++ numpy append.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+
+
+# Appending multiple values
+values_to_append = [7, 8, 9]
+new_arr = np.append(arr, values_to_append)
+
+print("Array after appending multiple values:", new_arr)
